5fcdca1b6c77e34bbe4bcd93d7df866d5beb1c9a
srini moola <srini moola>
2016-07-17 13:06:21 -0700

    mongo

--- db/db.js
+++ db/db.js
@@ -2,8 +2,11 @@
 ///<reference path="../typings/mongoose/mongoose.d.ts" />
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/social', function()
+mongoose.connect('mongodb://localhost/social', function(err)
 {
+    if (err) {
+        throw err
+    }
     console.log("DB connected")
 })
 
